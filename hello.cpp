062645b9afea76cf3f4d5f06352364a6cc7e508b
bobsira <sbobfitz2@gmail.com>
2017-10-17 10:50:57 +0300

    Commenting your programs

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,10 @@
 int main ()
 {
 
-	std::cout << "Hey, you, I'm alive! Oh, and Hello world!" << std::endl;
+	//std::cout << "Hey, you, I'm alive! Oh, and Hello world!" << std::endl;
+	// this is a one line comment
+
+	/* this is a multi-line comment
+	This is part of the comment
+	*/
 }
