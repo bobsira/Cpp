4c1201066dbbae5394719966a3125545e5714e03
bobsira <sbobfitz2@gmail.com>
2017-10-17 11:25:51 +0300

    Using variables

---
+++ readnum.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(int argc, char const *argv[]) {
+  int thisisanumber;
+  std::cout << "Please enter a number: " << '\n';
+  std::cin >> thisisanumber;
+  std::cout << "You entered: " << thisisanumber << '\n';
+  return 0;
+}
