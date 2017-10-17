f0d94a5c6766997b3e735429500274575b1e5aa5
bobsira <sbobfitz2@gmail.com>
2017-10-17 11:54:28 +0300

    appending one string onto another

--- string_name.cpp
+++ string_name.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 #include<string>
 int main(int argc, char const *argv[]) {
-  std::string user_name;
+  std::string user_first_name;
+  std::string user_last_name;
 
-  std::cout << "Please enter your name: ";
-  std::cin >> user_name;
-  std::cout << "Hi " << user_name << '\n';
+  std::cout << "Please enter your first name: ";
+  std::cin >> user_first_name;
+  std::cout << "Please enter your last name:  ";
+  std::cin >> user_last_name;
+
+  std::string user_full_name = user_first_name + " " + user_last_name;
+  std::cout << "Your name is " << user_full_name << '\n';
   return 0;
 }
