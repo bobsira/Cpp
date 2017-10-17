7de449acdfc9e68210440edd46aa04e9b32a549b
bobsira <sbobfitz2@gmail.com>
2017-10-17 11:08:44 +0300

    Practice problems Chap2

---
+++ chapter2/question1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main(int argc, char const *argv[]) {
+  /* code */
+  std::cout << "Bob Sira Sira" << '\n';
+  return 0;
+}
---
+++ chapter2/question2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(int argc, char const *argv[]) {
+  /* code */
+  std::cout << "Richard Muuo" << '\n';
+  std::cout << "Obadiah Obare" << '\n';
+  std::cout << "Stella Kaniaru" << '\n';
+  return 0;
+}
