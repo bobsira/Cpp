5b63dbf63433cda5b10fcc38424b4cb242e947ab
bobsira <sbobfitz2@gmail.com>
2017-10-27 00:44:03 +0300

    Else-if

--- non_negative.cpp
+++ non_negative.cpp
@@ -5,6 +5,8 @@ int main(int argc, char const *argv[]) {
   std::cin >> x;
   if (x<0) {
     std::cout << "You've entered a negative number " << '\n';
+  } else if(x == 0){
+    std::cout << "You've entered zero" << '\n';
   } else {
     std::cout << "You've entered a non-negative number" << '\n';
   }
