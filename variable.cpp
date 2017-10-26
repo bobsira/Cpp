c5e8cc66c6b0f662d79e8bea04c9ece9a9eec5ea
bobsira <sbobfitz2@gmail.com>
2017-10-27 00:31:50 +0300

    The bool type

--- variable.cpp
+++ variable.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 int main(int argc, char const *argv[]) {
   int x;
+  int y;
+
   std::cout << "Enter a number: " << '\n';
   std::cin >> x;
+  std::cout << "Enter another number " << '\n';
+  std::cin >> y;
+
   if (x<10) {
     std::cout << x << " is less than 10" << '\n';
   }
+
+  bool is_y_two = (y == 2); /* == for comparison*/
+  if (is_y_two) {
+    std::cout << "The number entered is " << y   << '\n';
+  }
+
   return 0;
 }
