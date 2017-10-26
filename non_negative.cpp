49c266de8b22255b06f554cf77ffdc4c8d4a2c94
bobsira <sbobfitz2@gmail.com>
2017-10-27 00:37:43 +0300

    Else statements

---
+++ non_negative.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(int argc, char const *argv[]) {
+  int x;
+  std::cout << "Enter a number " << '\n';
+  std::cin >> x;
+  if (x<0) {
+    std::cout << "You've entered a negative number " << '\n';
+  } else {
+    std::cout << "You've entered a non-negative number" << '\n';
+  }
+  return 0;
+}
