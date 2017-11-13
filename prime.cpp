aa2317f400c3f807329ac78916d85804dc61db9d
bobsira <sbobfitz2@gmail.com>
2017-11-13 18:58:21 +0300

    prime numbers

---
+++ prime.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+bool isDivisible(int number, int divisor);
+bool isPrime(int number);
+int main(int argc, char const *argv[]) {
+  for (int i = 0; i < 100; i++) {
+    if (isPrime(i)) {
+      std::cout << i << '\n';
+    }
+  }
+  return 0;
+}
+bool isPrime(int number){
+  for (int i = 2; i < number; i++) {
+    if (isDivisible(number,i)) {
+      return false;
+    }
+  }
+  return true;
+}
+bool isDivisible(int number,int divisor){
+  return number % divisor == 0 ;
+}
