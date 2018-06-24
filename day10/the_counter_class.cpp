6cb4f68a7136d12abb23de91cc66cacdcc35af4a
bobsira <sbobfitz2@gmail.com>
2018-06-24 07:29:25 +0300

    Operator Overloading

---
+++ day10/the_counter_class.cpp
@@ -0,0 +1,22 @@
+// Listing 10.6 - The Counter class
+#include <iostream>
+class Counter {
+private:
+  int itsVal;
+
+public:
+  Counter ();
+  ~Counter (){};
+  int GetItsVal() const { return itsVal;}
+  void SetItsVal(int x) { itsVal = x; }
+};
+
+Counter::Counter():
+itsVal(0)
+{}
+
+int main(int argc, char const *argv[]) {
+  Counter i;
+  std::cout << "The value of i is " << i.GetItsVal() << std::endl;
+  return 0;
+}
