40063f398d9b6345a7dccc5d8212aea7b3ce9cd2
bobsira <sbobfitz2@gmail.com>
2018-06-24 07:34:31 +0300

    Writing an Increment Function

--- day10/the_counter_class.cpp
+++ day10/the_counter_class.cpp
@@ -9,6 +9,7 @@ public:
   ~Counter (){};
   int GetItsVal() const { return itsVal;}
   void SetItsVal(int x) { itsVal = x; }
+  void Increment(){++itsVal;}
 };
 
 Counter::Counter():
@@ -17,6 +18,8 @@ itsVal(0)
 
 int main(int argc, char const *argv[]) {
   Counter i;
+  std::cout << "The value of i is " << i.GetItsVal() << std::endl;
+  i.Increment();
   std::cout << "The value of i is " << i.GetItsVal() << std::endl;
   return 0;
 }
